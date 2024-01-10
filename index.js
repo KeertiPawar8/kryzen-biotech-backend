4dfabeb84c636eb759860c3199a5dfdc53d0a4ad
keertiPawar8 <keertipawar8@gmail.com>
2024-01-10 12:53:03 +0530

    backend updated

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ const { connection } = require("./db/db");
 const { userRouter } = require("./controllers/user.routes");
 const {authenticate} = require("./middlewares/auth.middleware");
 const { userDataRouter } = require("./controllers/userData.routes");
+
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true); // if using credentials
+
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/user", userRouter);
