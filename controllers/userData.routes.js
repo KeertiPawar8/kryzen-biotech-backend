2a598639e2a76adb880dfe22219977b509a5c104
keertiPawar8 <keertipawar8@gmail.com>
2024-01-10 13:16:12 +0530

    backend updated

--- controllers/userData.routes.js
+++ controllers/userData.routes.js
@@ -14,12 +14,10 @@ cloudinary.config({
   });
   
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    return cb(null, "./uploads");
-  },
+
   filename: function (req, file, cb) {
     return cb(null, `${Date.now()}-${file.originalname}`);
-  },
+  }
 });
 const upload = multer({ storage });
 
